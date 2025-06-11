89e090d22eca3e597da81f1ae02d0f89694a1bdd
thekhuong97 <thekhuong97@gmail.com>
2025-06-11 16:20:27 +0700

    focus input

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ const $$ = document.querySelectorAll.bind(document);
 let addBtn = $(".add-btn");
 let closeModalBtn = $(".modal-close");
 let addTaskModal = $(".modal-overlay");
+let inputElement = $(".form-input");
 
 addBtn.onclick = function () {
   addTaskModal.className = "modal-overlay show";
+  setTimeout(() => {
+    inputElement.focus();
+  }, 100);
 };
 
 closeModalBtn.onclick = function () {
