7a5b885df1f3704dd5d3896c5baa733ec52785a6
thekhuong97 <thekhuong97@gmail.com>
2025-06-13 17:28:48 +0700

    complete task add

--- main.js
+++ main.js
@@ -1,101 +1,88 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-let addBtn = $(".add-btn");
-let closeModalBtn = $(".modal-close");
-let cancelModalBtn = $(".btn-cancel");
-let addTaskModal = $(".modal-overlay");
-let todoForm = $(".todo-app-form");
+const addBtn = $(".add-btn");
+const closeModalBtn = $(".modal-close");
+const cancelModalBtn = $(".btn-cancel");
+const addTaskModal = $("#addTaskModal");
+const todoForm = $(".todo-app-form");
+const titleInput = $("#taskTitle");
 
-let inputElement = $("#taskTitle.form-input");
-let textAreaElement = $(".form-textarea");
-let categoryElement = $("#taskCategory.form-select");
-let priorityElement = $("#taskPriority.form-select");
-let startTimeElement = $("#startTime.form-input");
-let endTimeElement = $("#endTime.form-input");
-let taskDateElement = $("#taskDate.form-input");
-let taskColorElement = $("#taskColor.form-select");
-
-let newTask = {
-  title: "Học lập trình tại F8",
-  description: "Học lập trình là không ngừng bỏ cuộc",
-  category: "",
-  priority: "",
-  startTime: "20:00",
-  endTime: "00:00",
-  dueDate: "",
-  cardColor: "",
-  isCompleted: false,
-};
-
-let todoTasks = [];
-todoTasks.unshift(newTask);
-let taskList = $(".task-grid");
-let html = todoTasks
-  .map(
-    (task) =>
-      `
-  <div class="task-card green">
-  <div class="task-header">
-    <h3 class="task-title">${task.title}</h3>
-    <button class="task-menu">
-      <i class="fa-solid fa-ellipsis fa-icon"></i>
-      <div class="dropdown-menu">
-        <div class="dropdown-item">
-          <i class="fa-solid fa-pen-to-square fa-icon"></i>
-          Edit
-        </div>
-        <div class="dropdown-item complete">
-          <i class="fa-solid fa-check fa-icon"></i>
-          Mark as Active
-        </div>
-        <div class="dropdown-item delete">
-          <i class="fa-solid fa-trash fa-icon"></i>
-          Delete
-        </div>
-      </div>
-    </button>
-  </div>
-  <p class="task-description">
-    ${task.description}
-  </p>
-  <div class="task-time">${task.startTime} - ${task.endTime}</div>
-</div>
-    `
-  )
-  .join("");
-
-// Open Form + Focus Function
+// Hàm mở form & focus
 function openForm() {
   addTaskModal.className = "modal-overlay show";
   setTimeout(() => {
-    inputElement.focus();
+    titleInput.focus();
   }, 100);
 }
 
-// Close Form Function
+// Hàm đóng form
 function closeForm() {
   addTaskModal.className = "modal-overlay";
 }
 
-// Hiển thị Modal thêm mới + Focus input
+// Hiển thị modal thêm mới + focus input
 addBtn.onclick = openForm;
 
-openForm(); //Mở sẵn form khi vào trang web
-
-// Đóng Modal thêm mới
+// Xử lý đóng modal thêm mới
 closeModalBtn.onclick = closeForm;
 cancelModalBtn.onclick = closeForm;
 
-// Xử Lý Khi Form Submit
+const todoTasks = [];
+
+// Xử lý khi form submit
 todoForm.onsubmit = function (event) {
   event.preventDefault();
-  const formValues = Object.fromEntries(new FormData(todoForm));
-  console.log(formValues);
+
+  // Lấy toàn bộ form data (dữ liệu từ các input, textarea,...)
+  const newTask = Object.fromEntries(new FormData(todoForm));
+  newTask.isCompleted = false; // Mặc định task chưa được hoàn thành
+
+  //   Thêm dữ liệu vào đầu mảng
+  todoTasks.unshift(newTask);
+
+  // Reset form & đóng modal
+  todoForm.reset();
+  closeForm();
+
+  // Cập nhật giao diện
+  renderTasks(todoTasks);
 };
 
-function renderTasks() {
-  taskList.innerHTML = html;
+// Hàm render giao diện
+function renderTasks(tasks) {
+  const html = tasks
+    .map(
+      (task) => `
+            <div class="task-card ${task.color} ${
+        task.isCompleted ? "completed" : ""
+      }">
+          <div class="task-header">
+            <h3 class="task-title">${task.title}</h3>
+            <button class="task-menu">
+              <i class="fa-solid fa-ellipsis fa-icon"></i>
+              <div class="dropdown-menu">
+                <div class="dropdown-item">
+                  <i class="fa-solid fa-pen-to-square fa-icon"></i>
+                  Edit
+                </div>
+                <div class="dropdown-item complete">
+                  <i class="fa-solid fa-check fa-icon"></i>
+                  ${task.isCompleted ? "Mark as Active" : "Mark as Complete"}
+                </div>
+                <div class="dropdown-item delete">
+                  <i class="fa-solid fa-trash fa-icon"></i>
+                  Delete
+                </div>
+              </div>
+            </button>
+          </div>
+          <p class="task-description">${task.description}</p>
+          <div class="task-time">${task.startTime} - ${task.endTime}</div>
+        </div>
+    `
+    )
+    .join("");
+  const todoList = $("#todoList");
+  todoList.innerHTML = html;
 }
-
-renderTasks();
